2ed0f169adfe3e77b5e59479986f74f0e28ed105
Adrien <n.adrien@mun.ca>
2020-05-13 16:28:26 -0230

    chors: adds scorecard-item component

--- projects/scorecard/src/app/layouts/network/network-modules/network.module.ts
+++ projects/scorecard/src/app/layouts/network/network-modules/network.module.ts
@@ -4,13 +4,15 @@ import { SharedModule } from '../../../shared/shared-modules/shared.module';
 import { NetworkComponent } from '../network.component';
 import { NetworkRoutingModule } from '../network-modules/network-routing.module';
 import { ScorecardListComponent } from '../scorecard-list/scorecard-list.component';
+import { ScorecardItemComponent } from '../scorecard-list/scorecard-item/scorecard-item.component';
 
 
 
 @NgModule({
   declarations: [
     NetworkComponent,
-    ScorecardListComponent
+    ScorecardListComponent,
+    ScorecardItemComponent
   ],
 
   imports: [
@@ -20,7 +22,8 @@ import { ScorecardListComponent } from '../scorecard-list/scorecard-list.compone
 
   exports: [
     NetworkComponent,
-    ScorecardListComponent
+    ScorecardListComponent,
+    ScorecardItemComponent
   ]
 })
 export class NetworkModule { }
---
+++ projects/scorecard/src/app/layouts/network/scorecard-list/scorecard-item/scorecard-item.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-scorecard-item',
+  templateUrl: './scorecard-item.component.html',
+  styleUrls: ['./scorecard-item.component.scss']
+})
+export class ScorecardItemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
